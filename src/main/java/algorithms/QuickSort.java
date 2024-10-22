7398f1b5c517ffa8ed8e95e96523ec179d7f6b06
bruch-alex <alex.bruch@tutanota.com>
2024-10-22 16:41:14 +0200

    add first multithreading implementation

--- src/main/java/algorithms/QuickSort.java
+++ src/main/java/algorithms/QuickSort.java
@@ -14,40 +14,65 @@ public class QuickSort extends SortingAlgorithm {
 
     @Override
     public void sort() {
-        int SLEEP_DURATION = 25;
+        int SLEEP_DURATION = 10;
         Set<Integer> sortedIndices = new HashSet<>();
-        quickSort(0, array.size() - 1, sortedIndices, SLEEP_DURATION);
+        Thread t = NewQuickSortThread(0, array.size() - 1, sortedIndices, SLEEP_DURATION);
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         Utils.displayVerticalArray(array, -1, -1, sortedIndices);
     }
 
-    void quickSort(int start, int end, Set<Integer> sortedIndices, int sleepDuration) {
-        if (start < end) {
-            int pi = partition(start, end, sortedIndices, sleepDuration);
-            quickSort(start, pi - 1, sortedIndices, sleepDuration);
-            quickSort(pi + 1, end, sortedIndices, sleepDuration);
-        }
+    Thread NewQuickSortThread(int start, int end, Set<Integer> sortedIndices, int sleepDuration) {
+        return new Thread(() -> {
+            if (start < end) {
+                int pi = partition(start, end, sortedIndices, sleepDuration);
+                Thread left = NewQuickSortThread(start, pi - 1, sortedIndices, sleepDuration);
+                Thread right = NewQuickSortThread(pi + 1, end, sortedIndices, sleepDuration);
+
+                left.start();
+                right.start();
+
+                try {
+                    left.join();
+                    right.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     int partition(int start, int end, Set<Integer> sortedIndices, int sleepDuration) {
-        int pivot = array.get(end);
-        int pIndex = start;
+        int pivotValue = array.get(end);
+        int partitionIndex = start;
 
-        for (int i = start; i <= end - 1; i++) {
+        for (int i = start; i < end; i++) {
             Utils.displayVerticalArray(array, i, end, sortedIndices);
-            comparisonCount++;
-            if (array.get(i) <= pivot) {
-                Utils.swapHighlighted(array, pIndex, i, sortedIndices, sleepDuration);
+            if (array.get(i) <= pivotValue) {
+                Utils.swapHighlighted(array, partitionIndex, i, sortedIndices, sleepDuration);
                 swapCount++;
-                pIndex++;
+                partitionIndex++;
+                if (array.get(i) == pivotValue) sortedIndices.add(i);
             }
+
+            comparisonCount++;
         }
 
-        Utils.swapHighlighted(array, pIndex, end, sortedIndices, sleepDuration);
+        Utils.swapHighlighted(array, partitionIndex, end, sortedIndices, sleepDuration);
         swapCount++;
-        sortedIndices.add(pIndex);
-        sortedIndices.add(start);
-        sortedIndices.add(end);
-        return pIndex;
+        sortedIndices.add(partitionIndex);
+        if (start == end - 1) {
+            sortedIndices.add(start);
+            sortedIndices.add(end);
+        }
+
+        //sortedIndices.add(start);
+        //sortedIndices.add(end);
+        return partitionIndex;
     }
 
 }
